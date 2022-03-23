04ee8cc81cc2f49d21335c415de8127d3a0fa3d8
k-natsuyama <100003813+k-natsuyama@users.noreply.github.com>
2022-03-23 22:44:40 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //button complate
+  const complateButton = document.createElement("button");
+  complateButton.innerText = "完了";
+  complateButton.addEventListener("click", () => {
+    deleteFromIncomplateList(complateButton.parentNode);
+  });
+
+  //button delete
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //
+    deleteFromIncomplateList(deleteButton.parentNode);
+  });
+
+  //div in
+  div.appendChild(li);
+  div.appendChild(complateButton);
+  div.appendChild(deleteButton);
+
+  //incomplate-list
+  document.getElementById("incomplate-list").appendChild(div);
+};
+
+//delete list
+const deleteFromIncomplateList = (target) => {
+  document.getElementById("incomplate-list").removeChild(target);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
